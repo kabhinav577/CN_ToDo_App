e856c05e142535ac7e29e7887f7a74a28478e086
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-06-26 12:05:04 +0530

    styel apply

---
+++ script.js
@@ -0,0 +1,13 @@
+// DOM Elements
+const todoForm = document.querySelector('#todo-form');
+
+const todoList = document.querySelector('.todos');
+
+const totalTasks = document.querySelector('#total-tasks');
+
+const completedTasks = document.querySelector('#completed-tasks');
+
+const remainingTasks = document.querySelector('#remaining-tasks');
+
+const mainInput = document.querySelector('#todo-form input');
+
